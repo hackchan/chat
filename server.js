dc32e209a80c918c1646f3ee0a2b8dbe0da226ee
fabio rojas <hackchan77@localhost.localdomain>
2015-04-16 03:03:46 -0500

    elimino funciones anonimas y uso callback con funciones declaradas

--- server.js
+++ server.js
@@ -2,5 +2,18 @@
 
 const http = require('http')
 const port = process.env.PORT || 8080
-const server= http.createServer()
+const server= http.createServer(onRequest)
+
+server.on('request',onRequest)
+server.on('listening',onListening)
 server.listen(port)
+
+function onRequest (req, res){
+  res.end('Hello Future, Welcome to Present io.js')
+
+}
+function onListening(){
+  console.log('Server Running in port ' + port)
+}
+
+
